832be325e020de8f65adeb26fc82ad8570865c33
ildoc <il_doc@protonmail.com>
2020-06-11 14:29:09 +0200

    added date unit tests

--- src/Extensions/DateTimeExtensions.cs
+++ src/Extensions/DateTimeExtensions.cs
@@ -2,7 +2,7 @@
 
 namespace Extensions
 {
-    public static class DateTimeExstensions
+    public static class DateTimeExtensions
     {
         public static DateTime DayStart(this DateTime date) => date.Date;
         public static DateTime DayEnd(this DateTime date) => date.Date + new TimeSpan(0, 23, 59, 59, 999);
