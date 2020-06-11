5b646b092af21c5583b991136e81d755807b8161
ildoc <il_doc@protonmail.com>
2020-06-11 14:29:09 +0200

    added date unit tests

---
+++ tests/Extensions.Tests/DateTimeExtensionsTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xunit;
+
+namespace Extensions.Tests
+{
+    public class DateTimeExtensionsTests
+    {
+        [Fact]
+        public void DayStartMinusOneMillisecondIsPreviousDay()
+        {
+            var date = DateTime.Now;
+
+            Assert.True(date.DayStart().AddMilliseconds(-1).Date == date.AddDays(-1).Date);
+        }
+
+        [Fact]
+        public void DayEndPlusOneMillisecondIsNextDay()
+        {
+            var date = DateTime.Now;
+
+            Assert.True(date.DayEnd().AddMilliseconds(1).Date == date.AddDays(1).Date);
+        }
+    }
+}
