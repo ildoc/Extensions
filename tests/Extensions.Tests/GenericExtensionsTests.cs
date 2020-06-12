65156df52ad01be54c32b37b2d9a29f7f330b96e
ildoc <il_doc@protonmail.com>
2020-06-12 16:30:45 +0200

    generic tests

---
+++ tests/Extensions.Tests/GenericExtensionsTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace Extensions.Tests
+{
+    public class GenericExtensionsTests
+    {
+        [Theory]
+        [InlineData('b', "abcd", true)]
+        [InlineData('f', "abcd", false)]
+        public void ShouldCheckIfValueIsInString(char value, string fullString, bool expected)
+        {
+            Assert.Equal(expected, value.IsIn(fullString));
+        }
+
+        [Theory]
+        [InlineData('b', "abcd", false)]
+        [InlineData('f', "abcd", true)]
+        public void ShouldCheckIfValueIsNotInString(char value, string fullString, bool expected)
+        {
+            Assert.Equal(expected, value.IsNotIn(fullString));
+        }
+    }
+}
