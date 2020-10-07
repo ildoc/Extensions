e88f88c03774240dea6818110c23f1c92566e46a
ildoc <il_doc@protonmail.com>
2020-10-07 11:45:48 +0200

    update

--- src/Extensions/ICollectionExtensions.cs
+++ src/Extensions/ICollectionExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Extensions
 {
@@ -25,6 +26,22 @@ namespace Extensions
             }
 
             return false;
+        }
+
+        public static void TryUpdateManyToMany<T>(this ICollection<T> currentItems, ICollection<T> newItems, Func<T,T,bool> comparer) where T : class
+        {
+            var toRemove = currentItems.Except(newItems, comparer).ToList();
+            var toAdd = newItems.Except(currentItems, comparer).ToList();
+
+            foreach (var item in toRemove)
+            {
+                currentItems.Remove(item);
+            }
+
+            foreach (var item in toAdd)
+            {
+                currentItems.Add(item);
+            }
         }
     }
 }
