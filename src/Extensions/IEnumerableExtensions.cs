66a620c169df334564e615315308b85d399a0cb2
ildoc <il_doc@protonmail.com>
2021-01-15 11:58:45 +0100

    fix ienumerable join

--- src/Extensions/IEnumerableExtensions.cs
+++ src/Extensions/IEnumerableExtensions.cs
@@ -12,11 +12,11 @@ namespace Extensions
 
         public static void Each<T>(this IEnumerable source, Action<T> action) => For.Each(source, action);
 
-        public static IEnumerable<T> Except<T>(this IEnumerable<T> list, IEnumerable<T> except, Func<T,T, bool> comparer) => 
+        public static IEnumerable<T> Except<T>(this IEnumerable<T> list, IEnumerable<T> except, Func<T, T, bool> comparer) =>
             list.Except(except, new KeyEqualityComparer<T>(comparer));
 
-        public static string Join(this IEnumerable source, char separator) => string.Join(separator, source);
-        public static string Join(this IEnumerable source, string separator) => string.Join(separator, source);
+        public static string Join<T>(this IEnumerable<T> source, char separator) => string.Join(separator, source);
+        public static string Join<T>(this IEnumerable<T> source, string separator) => string.Join(separator, source);
 
         public static IEnumerable<T> WhereIf<T>(this IEnumerable<T> enumerable, bool condition, Func<T, bool> f) =>
            condition ? enumerable.Where(f) : enumerable;
---
+++ tests/Extensions.Tests/IEnumerableExtensionsTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace Extensions.Tests
+{
+    public class IEnumerableExtensionsTests
+    {
+        [Fact]
+        public void ShouldJoinListString()
+        {
+            var list = new List<string> { "questa", "è", "una", "lista" };
+            Assert.Equal("questa, è, una, lista", list.Join(", "));
+        }
+
+        [Fact]
+        public void ShouldJoinListChar()
+        {
+            var list = new List<string> { "questa", "è", "una", "lista" };
+            Assert.Equal("questa,è,una,lista", list.Join(','));
+        }
+    }
+}
