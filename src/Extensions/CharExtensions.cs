cbd2cb62f7062d5d727f1938d17e17645da24ef0
Filippo <il_doc@protonmail.com>
2022-03-16 13:42:00 +0000

    Char tests (#8)
    * added char tests and documentation

--- src/Extensions/CharExtensions.cs
+++ src/Extensions/CharExtensions.cs
@@ -2,9 +2,17 @@
 {
     public static class CharExtensions
     {
+        /// <summary>
+        /// Transforms the char to lowercase
+        /// </summary>
+        /// <param name="c"></param>
         public static char ToLower(this char c) =>
             char.ToLower(c);
 
+        /// <summary>
+        /// Transforms the char to uppercase
+        /// </summary>
+        /// <param name="c"></param>
         public static char ToUpper(this char c) =>
             char.ToUpper(c);
 
