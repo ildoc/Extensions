1b7d6d53ac19c38d37cd5f8a12da076f2c63b132
Filippo <il_doc@protonmail.com>
2022-03-16 13:42:00 +0000

    Char tests (#8)
    * added char tests and documentation

---
+++ tests/Extensions.Tests/CharExtensionsTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Extensions.Tests
+{
+    public class CharExtensionsTests
+    {
+        [Theory]
+        [InlineData('a','A')]
+        [InlineData('A','A')]
+        [InlineData(' ',' ')]
+        public void ShouldTransformToUppercase(char value, char expected)
+        {
+            Assert.Equal(expected, value.ToUpper());
+        }
+
+        [Theory]
+        [InlineData('A', 'a')]
+        [InlineData('a', 'a')]
+        [InlineData(' ', ' ')]
+        public void ShouldTransformToLowercase(char value, char expected)
+        {
+            Assert.Equal(expected, value.ToLower());
+        }
+    }
+}
