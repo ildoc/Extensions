15423e7e5fcf3d686c923d14f27984b02d0270cb
Matteo Musso <matteo.musso@bss.group>
2022-03-16 12:17:29 +0100

    Added new CharExtensions and DateTimeExtensions

--- src/Extensions/CharExtensions.cs
+++ src/Extensions/CharExtensions.cs
@@ -7,5 +7,13 @@
 
         public static char ToUpper(this char c) =>
             char.ToUpper(c);
+
+        /// <summary>
+        /// Checks if a char is [a-zA-Z]
+        /// </summary>
+        /// <param name="ch"></param>
+        /// <returns></returns>
+        public static bool IsAsciiAlphabetLetter(this char ch) =>
+            (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z');
     }
 }
