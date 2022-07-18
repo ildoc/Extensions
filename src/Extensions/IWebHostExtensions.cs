3ee0c4979ce4dbe6740128f654a2826a631b2b42
ildoc <il_doc@protonmail.com>
2022-07-18 19:50:51 +0200

    separated migrate and seed

--- src/Extensions/IWebHostExtensions.cs
+++ src/Extensions/IWebHostExtensions.cs
@@ -2,7 +2,6 @@
 using System.Data.SqlClient;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Polly;
@@ -11,46 +10,43 @@ namespace Extensions
 {
     public static class IWebHostExtensions
     {
-        public static bool IsInKubernetes(this IWebHost webHost)
+        public static IWebHost MigrateDbContext<TContext>(this IWebHost webHost) where TContext : DbContext
         {
-            var cfg = webHost.Services.GetService<IConfiguration>();
-            return CheckIfK8s(cfg);
-        }
-
-        private static bool CheckIfK8s(IConfiguration cfg)
-        {
-            var orchestratorType = cfg.GetValue<string>("OrchestratorType");
-            return orchestratorType?.ToUpper() == "K8S";
-        }
-
-        public static IWebHost MigrateDbContext<TContext>(this IWebHost webHost, Action<TContext, IServiceProvider> seeder) where TContext : DbContext
-        {
-            var underK8s = webHost.IsInKubernetes();
-
             using var scope = webHost.Services.CreateScope();
 
-            TryMigrateAndSeed(scope, underK8s, seeder);
+            TryMigrate<TContext>(scope);
 
             return webHost;
         }
 
-        public static bool IsInKubernetes(this Microsoft.Extensions.Hosting.IHost host)
+        public static Microsoft.Extensions.Hosting.IHost MigrateDbContext<TContext>(this Microsoft.Extensions.Hosting.IHost host) where TContext : DbContext
         {
-            var cfg = host.Services.GetService<IConfiguration>();
-            return CheckIfK8s(cfg);
+            using var scope = host.Services.CreateScope();
+
+            TryMigrate<TContext>(scope);
+
+            return host;
         }
 
-        public static Microsoft.Extensions.Hosting.IHost MigrateDbContext<TContext>(this Microsoft.Extensions.Hosting.IHost webHost, Action<TContext, IServiceProvider> seeder) where TContext : DbContext
+        public static IWebHost MigrateDbContext<TContext>(this IWebHost webHost, Action<TContext, IServiceProvider> seeder = default) where TContext : DbContext
         {
-            var underK8s = webHost.IsInKubernetes();
-
             using var scope = webHost.Services.CreateScope();
-            TryMigrateAndSeed(scope, underK8s, seeder);
+
+            TryMigrateAndSeed(scope, seeder);
 
             return webHost;
         }
 
-        private static void TryMigrateAndSeed<TContext>(IServiceScope scope, bool underK8s, Action<TContext, IServiceProvider> seeder) where TContext : DbContext
+        public static Microsoft.Extensions.Hosting.IHost MigrateDbContext<TContext>(this Microsoft.Extensions.Hosting.IHost host, Action<TContext, IServiceProvider> seeder = default) where TContext : DbContext
+        {
+            using var scope = host.Services.CreateScope();
+
+            TryMigrateAndSeed(scope, seeder);
+
+            return host;
+        }
+
+        private static void TryMigrateAndSeed<TContext>(IServiceScope scope, Action<TContext, IServiceProvider> seeder) where TContext : DbContext
         {
             var services = scope.ServiceProvider;
 
@@ -62,36 +58,59 @@ namespace Extensions
             {
                 logger.LogInformation("Migrating database associated with context {DbContextName}", typeof(TContext).Name);
 
-                if (underK8s)
-                {
-                    InvokeSeeder(seeder, context, services);
-                }
-                else
-                {
-                    var retry = Policy.Handle<SqlException>()
-                         .WaitAndRetry(new TimeSpan[]
-                         {
+
+                var retry = Policy.Handle<SqlException>()
+                     .WaitAndRetry(new TimeSpan[]
+                     {
                              TimeSpan.FromSeconds(3),
                              TimeSpan.FromSeconds(5),
                              TimeSpan.FromSeconds(8),
-                         });
+                     });
 
-                    //if the sql server container is not created on run docker compose this
-                    //migration can't fail for network related exception. The retry options for DbContext only 
-                    //apply to transient exceptions
-                    // Note that this is NOT applied when running some orchestrators (let the orchestrator to recreate the failing service)
-                    retry.Execute(() => InvokeSeeder(seeder, context, services));
-                }
+                //if the sql server container is not created on run docker compose this
+                //migration can't fail for network related exception. The retry options for DbContext only 
+                //apply to transient exceptions
+                retry.Execute(() => InvokeSeeder(seeder, context, services));
 
                 logger.LogInformation("Migrated database associated with context {DbContextName}", typeof(TContext).Name);
             }
             catch (Exception ex)
             {
                 logger.LogError(ex, "An error occurred while migrating the database used on context {DbContextName}", typeof(TContext).Name);
-                if (underK8s)
-                {
-                    throw;          // Rethrow under k8s because we rely on k8s to re-run the pod
-                }
+            }
+        }
+
+        private static void TryMigrate<TContext>(IServiceScope scope) where TContext : DbContext
+        {
+            var services = scope.ServiceProvider;
+
+            var logger = services.GetRequiredService<ILogger<TContext>>();
+
+            var context = services.GetService<TContext>();
+
+            try
+            {
+                logger.LogInformation("Migrating database associated with context {DbContextName}", typeof(TContext).Name);
+
+
+                var retry = Policy.Handle<SqlException>()
+                     .WaitAndRetry(new TimeSpan[]
+                     {
+                             TimeSpan.FromSeconds(3),
+                             TimeSpan.FromSeconds(5),
+                             TimeSpan.FromSeconds(8),
+                     });
+
+                //if the sql server container is not created on run docker compose this
+                //migration can't fail for network related exception. The retry options for DbContext only 
+                //apply to transient exceptions
+                retry.Execute(() => context.Database.Migrate());
+
+                logger.LogInformation("Migrated database associated with context {DbContextName}", typeof(TContext).Name);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "An error occurred while migrating the database used on context {DbContextName}", typeof(TContext).Name);
             }
         }
 
