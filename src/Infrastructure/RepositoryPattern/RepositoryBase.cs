5a1a9a2836276a472312fd460bedaa14de8681fb
ildoc <il_doc@protonmail.com>
2022-11-03 11:17:02 +0100

    small fixes

--- src/Infrastructure/RepositoryPattern/RepositoryBase.cs
+++ src/Infrastructure/RepositoryPattern/RepositoryBase.cs
@@ -1,26 +1,25 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Extensions;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.RepositoryPattern
 {
     public class RepositoryBase<T> : IRepository<T> where T : class
     {
         private readonly DbContext _context;
-        private DbSet<T> _dbSet { get; }
+        private DbSet<T> DbSet { get; }
 
         public RepositoryBase(DbContext context)
         {
             _context = context;
-            _dbSet = _context.Set<T>();
+            DbSet = _context.Set<T>();
         }
 
         public IQueryable<T> Query(bool trackChanges = true) =>
-            !trackChanges ?
-              _dbSet.AsNoTracking() :
-              _dbSet;
+              DbSet.AsNoTrackingIf(!trackChanges);
 
         public virtual T GetById<TId>(TId id, bool track = true)
         {
-            var entity = _dbSet.Find(id);
+            var entity = DbSet.Find(id);
             if (!track)
                 _context.Entry(entity).State = EntityState.Detached;
             return entity;
@@ -28,21 +27,21 @@ namespace Infrastructure.RepositoryPattern
 
         public virtual async Task<T> GetByIdAsync<TId>(TId id, bool track = true)
         {
-            var entity = await _dbSet.FindAsync(id);
+            var entity = await DbSet.FindAsync(id);
             if (!track)
                 _context.Entry(entity).State = EntityState.Detached;
             return entity;
         }
 
-        public void Create(T entity) => _dbSet.Add(entity);
+        public void Create(T entity) => DbSet.Add(entity);
 
-        public void Update(T entity) => _dbSet.Update(entity);
-        public void UpdateRange(params T[] entities) => _dbSet.UpdateRange(entities);
-        public void UpdateRange(IEnumerable<T> entities) => _dbSet.UpdateRange(entities);
+        public void Update(T entity) => DbSet.Update(entity);
+        public void UpdateRange(params T[] entities) => DbSet.UpdateRange(entities);
+        public void UpdateRange(IEnumerable<T> entities) => DbSet.UpdateRange(entities);
 
-        public void Remove(T entity) => _dbSet.Remove(entity);
-        public void RemoveRange(params T[] entities) => _dbSet.RemoveRange(entities);
-        public void RemoveRange(IEnumerable<T> entities) => _dbSet.RemoveRange(entities);
+        public void Remove(T entity) => DbSet.Remove(entity);
+        public void RemoveRange(params T[] entities) => DbSet.RemoveRange(entities);
+        public void RemoveRange(IEnumerable<T> entities) => DbSet.RemoveRange(entities);
 
         public virtual async void RemoveByIdAsync<TId>(TId id)
         {
@@ -54,7 +53,7 @@ namespace Infrastructure.RepositoryPattern
             if (entityToDelete == null)
                 throw new Exception("Entity not found");
 
-            _dbSet.Remove(entityToDelete);
+            DbSet.Remove(entityToDelete);
         }
 
         public virtual void RemoveById<TId>(TId id)
@@ -67,7 +66,7 @@ namespace Infrastructure.RepositoryPattern
             if (entityToDelete == null)
                 throw new Exception("Entity not found");
 
-            _dbSet.Remove(entityToDelete);
+            DbSet.Remove(entityToDelete);
         }
     }
 }
